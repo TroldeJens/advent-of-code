4838e2badf9f552aed7feccf50606fce2605c2c3
TroldeJens <jens.laursen@lego.com>
2023-12-05 22:37:19 +0100

    feat: Added 2b and refactored 2a

--- 2023/02.py
+++ 2023/02.py
@@ -7,11 +7,6 @@ input_filename  = "02_input.txt"
 ## Other
 debug           = False
 
-## Variables
-max_red = 12
-max_green = 13
-max_blue = 14
-
 import os
 from os import path
 import re
@@ -27,36 +22,22 @@ def get_input(filename: str) -> list[str]:
         lines = fileinput.read().splitlines()
     
     return lines
-    
-def is_pick_valid(id: str, picks: list) -> bool:
-    """Determine whether a given set of picks are valid or not."""
-    for amount, colour in picks:
-        match colour:
-            case "red": 
-                if int(amount) > max_red:
-                    if debug:
-                        print(f"Id: {id}. Red is too large: {amount}")
-                    return False
-            case "green": 
-                if int(amount) > max_green:
-                    if debug:
-                        print(f"Id: {id}. Green is too large: {amount}")
-                    return False
-            case "blue": 
-                if int(amount) > max_blue:
-                    if debug:
-                        print(f"Id: {id}. Blue is too large: {amount}")
-                    return False
-    return True
 
 def run_a():
     """Run assignment a."""
-    total = 0
+
+    max_red = 12
+    max_green = 13
+    max_blue = 14
 
     lines = get_input(input_filename)
+
+    total = 0
     for line in lines:
         ids     = re.findall(r'Game (\d+)', line)
-        picks   = re.findall(r'(\d+) (red|green|blue)', line)
+        reds    = re.findall(r'(\d+) red', line)
+        greens  = re.findall(r'(\d+) green', line)
+        blues   = re.findall(r'(\d+) blue', line)
 
         ## Skip, if no ids were located
         if not ids:
@@ -64,22 +45,66 @@ def run_a():
             continue
 
         ## Skip, if neither reds, greens nor blues were located
-        if not picks:
+        if not reds and not greens and not blues:
             print(f"There are neither reds, greens or blues in line: {line}")
             continue
 
         id = str(ids[0])
 
-        if not is_pick_valid(id, picks):
+        ## Convert strings to int
+        reds    = [int(value) for value in reds]
+        greens  = [int(value) for value in greens]
+        blues   = [int(value) for value in blues]
+
+        ## Skip, if max of any colour exceeds its max_red/max_blue/max_green
+        if max(reds) > max_red or max(greens) > max_green or max(blues) > max_blue:
+            if debug:
+                print(f"IMPOSSIBLE GAME: Id: {id}. Reds: {reds.__str__()}. Greens: {greens.__str__()}. Blues: {blues.__str__()}. Max reds: {max(reds)}. Max greens: {max(greens)}. Max blues: {max(blues)}.")
             continue
+
+        if debug:
+            print(f"POSSIBLE GAME: Id: {id}. Reds: {reds.__str__()}. Greens: {greens.__str__()}. Blues: {blues.__str__()}. Max reds: {max(reds)}. Max greens: {max(greens)}. Max blues: {max(blues)}.")
 
         total += int(id)
                 
     print(f"Total: {total}")
 
 def run_b():
-    """Run assignment b - I really didn't understand this one"""
-    print("UNFINISHED")
+    """Run assignment b"""
+    total = 0
+
+    lines = get_input(input_filename)
+    for line in lines:
+        ids     = re.findall(r'Game (\d+)', line)
+        reds    = re.findall(r'(\d+) red', line)
+        greens  = re.findall(r'(\d+) green', line)
+        blues   = re.findall(r'(\d+) blue', line)
+
+        ## Skip, if no ids were located
+        if not ids:
+            print(f"No id located in line: {line}")
+            continue
+
+        ## Skip, if neither reds, greens nor blues were located
+        if not reds and not greens and not blues:
+            print(f"There are neither reds, greens or blues in line: {line}")
+            continue
+
+        id = str(ids[0])
+
+        ## Convert strings to int
+        reds    = [int(value) for value in reds]
+        greens  = [int(value) for value in greens]
+        blues   = [int(value) for value in blues]
+
+        max_colours_multiplied = int(max(reds)) * int(max(greens)) * int(max(blues))
+
+        if debug:
+            print(f"id: {id}. Reds: {reds.__str__()}. Greens: {greens.__str__()}. Blues: {blues.__str__()}. Max reds: {max(reds)}. Max greens: {max(greens)}. Max blues: {max(blues)}. Maxs multiplied: {max_colours_multiplied}")
+
+        total += max_colours_multiplied
+
+    print(f"Total: {total}")
 
 ## Run program
 if __name__ == '__main__':
